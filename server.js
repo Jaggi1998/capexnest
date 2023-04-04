b0e72d87c5ebf775a19d5db1dc15ccaf26b8a18f
jaggy1998 <jagjots28@gmail.com>
2023-04-04 10:26:08 +0530

    newCide

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
